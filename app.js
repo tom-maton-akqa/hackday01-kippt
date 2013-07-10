45452d5d0ce62c35bd68b5c232a0a08b5cc5bb02
Owain Llewellyn <owain.llew@gmail.com>
2013-07-10 23:23:35 +0100

    updated procfile

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var nodemailer = require("nodemailer");
 var smtpTransport = nodemailer.createTransport("SMTP",{
    service: "Gmail",
    auth: {
-       user: "XXXX",
-       pass: "XXXX-"
+       user: "owain.llew@gmail.com",
+       pass: "7321SK8on7321-"
    }
 });
 
 smtpTransport.sendMail({
    from: "<me@example.com>", // sender address
-   to: "<XXXX>", // comma separated list of receivers
+   to: "owain.llew@gmail.com", // comma separated list of receivers
    subject: "Node SMTP Server", // Subject line
    text: "If you guys are receieving this then I have successfully created a node SMTP Server! :)" // plaintext body
 }, function(error, response){
